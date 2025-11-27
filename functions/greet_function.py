8fdaeece7eb3da1a3caa64ed30b4cfd674da22ce
Vaishnavikaple <vaish.kaple2001@gmail.com>
2025-11-27 18:50:08 +0530

    updated function

---
+++ functions/greet_function.py
@@ -0,0 +1,16 @@
+def greet():
+    name = input("Enter your name here : " )
+    print(f"Hello {name} Good Morning!")
+    print("Welcome To Python Course")
+
+
+
+
+def good_bye():
+    name = input("Enter your name here :")
+    print(f"Good Bye {name.upper()}")
+    print("See You Later")
+
+good_bye()
+    
+
