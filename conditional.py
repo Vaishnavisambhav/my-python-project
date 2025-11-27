1c5e90af23f899e0f0c4675bb4bb743ae514780e
Vaishnavikaple <vaish.kaple2001@gmail.com>
2025-11-27 12:30:33 +0530

    new

--- conditional.py
+++ conditional.py
@@ -3,3 +3,19 @@ if your_age >= 18:
     print("You can drive a car")        
 else:
     print("You cannot drive a car")
+
+
+your_score = 40
+
+if your_score>=90:
+    print("A = Excellent")
+elif your_score>=80:
+    print("B = Very Good")      
+elif your_score>=70:
+    print("C = Good")
+elif your_score>=60:
+    print("D = Average")
+elif your_score>=50:
+    print("E = Below Average")
+else:
+    print("F = Need Improvement")
