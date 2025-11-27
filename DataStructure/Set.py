52c42b595ccd34d9f478fdea5827f69c8ea0a6ea
Vaishnavikaple <vaish.kaple2001@gmail.com>
2025-11-27 18:10:41 +0530

    set

---
+++ DataStructure/Set.py
@@ -0,0 +1,7 @@
+numbers={1,2,3,4,5,6}#directly creating a set
+
+List_of_fruits = ["banana","mango","orange"] #created list 
+set_of_fruits = set(List_of_fruits) #passing the list in set
+
+tuple = (23,33,3333)
+print(set(tuple))#passing tuple and converting to set
---
+++ DataStructure/Tuple.py
@@ -0,0 +1,12 @@
+# Tuples are Immutable
+ids = (101,201,301,401)
+colors = ("red","orange","yellow")
+
+print(ids[0])
+print(ids[3])
+print(colors[-1])
+
+#colors[0]="blues" #will show error
+
+
+
