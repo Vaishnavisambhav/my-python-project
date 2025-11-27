554792a196641409fb5511d0e159a6cdc4d825d4
Vaishnavikaple <vaish.kaple2001@gmail.com>
2025-11-27 17:17:12 +0530

    Updated Calculator

--- Calcultor-using-python/calculator.py
+++ Calcultor-using-python/calculator.py
@@ -1,46 +1,49 @@
-def add(a, b):
-    return a + b
+def add(a,b):
+    return a+b
 
-def subtract(a, b):
-    return a - b
+def sub(a,b):
+    return a-b
 
-def multiply(a, b):
-    return a * b
+def mul(a,b):
+    return a*b
 
-def divide(a, b):
+def div(a,b):
     if b == 0:
-        return "Error! Cannot divide by zero."
-    return a / b
+        return"Error, cannot divide by 0"
+    return a/b
+dash = (10 * "=")
 
 while True:
-    print("\n===== SIMPLE CALCULATOR =====")
+    print(f"{dash }Simple Calculator {dash}")
     print("1. Add")
     print("2. Subtract")
     print("3. Multiply")
     print("4. Divide")
     print("5. Exit")
 
-    choice = input("Enter your choice (1-5): ")
+    choice = input("Enter the choice (1-5) : ")
 
-    if choice == "5":
-        print("Thank you! Exiting calculator.")
+
+    if choice =="5":
+        print("Thank You! Exiting Calculator ")
         break
 
-    if choice not in ["1", "2", "3", "4"]:
-        print("Invalid choice! Try again.")
+    if choice not in ("1","2","3","4"):
+        print("Invalid Choice ")
         continue
 
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
+    num1 = float(input("Enter the first number : "))
+    num2 = float(input("Enter the second number : "))
 
-    if choice == "1":
-        print("Result:", add(num1, num2))
+    if(choice == "1"):
+        print ("The addition of the numbers is :" , add(num1,num2))
 
-    elif choice == "2":
-        print("Result:", subtract(num1, num2))
+    elif(choice == "2" ):
+        print("the substraction of the numbers is : " , sub(num1,num2))
 
-    elif choice == "3":
-        print("Result:", multiply(num1, num2))
+    elif(choice == "3" ):
+        print("the substraction of the numbers is : " , mul(num1,num2))
 
     elif choice == "4":
-        print("Result:", divide(num1, num2))
+        print("Result:", div(num1, num2))
+
